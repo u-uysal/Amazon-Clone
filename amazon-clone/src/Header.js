83b55ac7aa8ee74a4ec929b8a9ca843b4c6a764a
ufuk <ufuk.uysl5@gmail.com>
2020-09-29 14:13:03 +0200

    configuration and router

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="header__logo" src="" alt="logo" />
+    </div>
+  );
+}
